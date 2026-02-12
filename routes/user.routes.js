f66799e450b6215c74855cac29dbf8c89536e861
nitin kumar <nitin981275@gmail.com>
2026-02-12 21:06:33 +0530

    Add GET /users and GET /users/:id with proper status handling

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -25,8 +25,63 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get("/", (req, res) => {
-  res.send("this is working properly, but why not post?!!");
+// READ ALL USERS FORM .GET
+router.get("/", async (req, res) => {
+  try {
+    const user = await User.find();
+
+    res.status(200).json({
+      message: "All User data.",
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
 });
+
+// GET SINGLE USER BY ID
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "User fetched successfully",
+      data: user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Invalid user ID",
+    });
+  }
+});
+
+//UPDATE USER
+// router.put("/:id", async (req, res) => {
+//   try {
+//     const id = req.params.id;
+//     const user = User.findById(id);
+//     if (!user) {
+//       return res.status(400).json({
+//         message: "there is not any user",
+//       });
+//     }
+//     req.body = user;
+//     res.status(200).json({
+//       message: "User Updated successfully.",
+//       data: user,
+//     });
+//   } catch (error) {
+//     console.error("error occurs:", error.message);
+//   }
+// });
 
 export default router;
