547d2e575263370a733fcbacd9168ca1be9f0e3b
nitin kumar <nitin981275@gmail.com>
2026-02-13 15:05:40 +0530

    Add PUT /users/:id to update user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -86,4 +86,26 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+// DELETE
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await User.findByIdAndDelete(id);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User does not exist",
+      });
+    }
+    res.status(200).json({
+      message: "User Deleted.",
+      data: user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "invalid uder id",
+    });
+  }
+});
+
 export default router;
